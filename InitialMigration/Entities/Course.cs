b160d79a4c529703e8b258dfb4505cb334c11abf
Mohamed Hamada <moha2016226@gmail.com>
2024-10-30 15:32:12 -0700

    create Section Entity With relation One To Many With Course and Instructor

--- InitialMigration/Entities/Course.cs
+++ InitialMigration/Entities/Course.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string? CourseName { get; set; }
         public decimal Price { get; set; }
+        public ICollection<Section> Sections { get; set; } = new List<Section>();
     }
 }
--- InitialMigration/Entities/Instructor.cs
+++ InitialMigration/Entities/Instructor.cs
@@ -7,6 +7,7 @@
         public string? LName { get; set; }
         public int? OfficeId { get; set; }
         public Office? Office { get; set; }
+        public ICollection<Section> Sections { get; set; } = new List<Section>();
 
 
     }
